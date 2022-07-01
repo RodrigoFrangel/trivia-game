dc7b9e5bf98273b70f3271f69e80a1642db18118
ThalesFerreira <thalesferreira190@gmail.com>
2022-06-30 23:03:34 -0300

    Requisito 17 - incompleto

---
+++ src/mocks/mocks.js
@@ -0,0 +1,18 @@
+const responseGlobalState = {
+  results: [
+    {
+      name: 'Fulano de Tal',
+    },
+    {
+      email: 'fulanodetal@gmail.com',
+    },
+    {
+      assertions: 2,
+    },
+    {
+      score: 110,
+    },
+  ],
+};
+
+export default responseGlobalState;
